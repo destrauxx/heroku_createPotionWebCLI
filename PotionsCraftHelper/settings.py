3d4dd3ccb5b43b55ca60b1fe40c02f97e5f2b9cf
paurbx <gleroapachii@gmail.com>
2021-10-26 19:03:38 +0300

    some error

--- PotionsCraftHelper/settings.py
+++ PotionsCraftHelper/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/staticbase/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS=[
     BASE_DIR/'staticbase',
